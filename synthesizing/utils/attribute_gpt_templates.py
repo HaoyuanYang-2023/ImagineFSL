0ad5101df9ff51a449694fb59fbb68b8cc1ec684
HaoyuanYang-2023 <yanghaoyuan@mail.dlut.edu.cn>
2025-03-21 14:36:12 +0800

    Update synthesizing code

--- synthesizing/utils/attribute_gpt_templates.py
+++ synthesizing/utils/attribute_gpt_templates.py
@@ -1,6 +1,6 @@
 origion_attribute = """Generate a list of 20 distinct visual attributes that distinguish this particular {} from the others while accommodating variations within the {} in everyday photographs. Present this list as a Python array, with each element being a concise phrase that describes a unique attribute."""
 
-origion_eurosat_attribute = """Generate a list of 20 distinct visual attributes that differentiate a/an {} from other {} in remote sensing images. **MUST Present this list as a Python array**, with each element being a concise phrase but distinctive that describes a unique attribute."""
+origion_eurosat_attribute = """Generate a list of 20 distinct visual attributes that distinguish this particular {} from other {} in remote sensing images. s. Present this list as a Python array, with each element being a concise phrase that describes a unique attribute."""
 
 origion_ucf_attribute = """Generate a list of 20 visual distinct steps that characterize the action of {} in everyday videos, which can differentiate this action from the other actions. Present this list as a Python array, with each element being a concise phrase that describes a unique step in the action sequence."""
 
